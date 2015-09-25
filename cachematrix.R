600285f8721314577587db69c7b018df8ac420ec
royph <olson.r.88@gmail.com>
2015-09-24 22:49:25 -0700

    assignment2 first sumbission attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions can be used to cache calculation
+## of the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function creates an object from a matrix.
+## The object contains a list of functions:
+##    set - sets the value of the matrix
+##    get - gets the value of the matrix
+##    setinverse - sets the value of the inverse of the matrix
+##    getinverse - gets the value of the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      setinv <- function(inverse) inv <<- inverse
+      getinv <- function() inv
+      list(set = set, get = get, 
+           setmean = setmean, 
+           getmean = getmean)
+  
 }
 
 
-## Write a short comment describing this function
+## This function checks an object created by makeCacheMatrix
+## for a previously calculated inverse matrix. If no inverse
+## matrix has been set, this function solves for the inverse
+## of the matrix set in makeCacheMatrix. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinv()
+      if(!is.null(inv)){
+            message("getting cached data")
+            return(inv)
+      }
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$setinv(inv)
+      inv
 }
